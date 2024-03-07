3353001f94c13386c24673ad289d0f9f09e217a0
Juan Miguel Duran <48862245+juanmix13@users.noreply.github.com>
2024-03-07 13:43:13 +0100

    Files from Spring Initializer
    Files from Spring Initializer

---
+++ src/main/java/com/learning/messagesender/MessageSenderApplication.java
@@ -0,0 +1,13 @@
+package com.learning.messagesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSenderApplication.class, args);
+	}
+
+}
